094ec473e4816f083f82f41fc8076dfc44354b59
cheng <481193684@qq.com>
2023-11-28 19:35:15 +0800

    feature(all):完成 gulimall 项目的 nacos 配置

--- gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/GulimallCouponApplication.java
+++ gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/GulimallCouponApplication.java
@@ -1,15 +1,13 @@
-package com.atguigu.gulimall.product;
+package com.atguigu.gulimall.coupon;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@MapperScan("com.atguigu.gulimall.product.dao")
 @SpringBootApplication
 @EnableDiscoveryClient
-public class GulimallProductApplication {
+public class GulimallCouponApplication {
     public static void main(String[] args) {
-        SpringApplication.run(GulimallProductApplication.class, args);
+        SpringApplication.run(GulimallCouponApplication.class, args);
     }
 }
--- gulimall-ware/src/test/java/com/atguigu/gulimall/ware/GulimallWareApplicationTests.java
+++ gulimall-ware/src/test/java/com/atguigu/gulimall/ware/GulimallWareApplicationTests.java
@@ -1,13 +1,15 @@
 package com.atguigu.gulimall.ware;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootTest
+@EnableDiscoveryClient
 class GulimallWareApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
 
+	}
 }
